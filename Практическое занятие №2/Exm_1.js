eda4a72680be828a8fd15465080911035ca86c84
Denis-Mosunov <dnsmosunov@gmail.com>
2025-04-09 20:18:19 +0300

    Update Exm_1.js

--- Практическое занятие №2/Exm_1.js
+++ Практическое занятие №2/Exm_1.js
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="ru">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>laba-2-1.html</title>
+    <title>lab-2-1.html</title>
     <style>
         img {
             transition: transform 0.3s ease;
@@ -12,8 +12,8 @@
     </style>
 </head>
 <body>
-    <h1>Кликните на изображение, чтобы изменить его размер</h1>
-    <img src="apple.jpg" alt="Пример изображения" id="resizableImage" width="300">
+    <h1>Click on the image to change its size</h1>
+    <img src="apple.jpg" alt="Example image" id="resizableImage" width="300">
 
     <script>
         const image = document.getElementById('resizableImage');
