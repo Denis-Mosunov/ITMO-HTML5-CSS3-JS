accbd9eccdce70c8f26ed8b898213a571e41a93a
Denis-Mosunov <dnsmosunov@gmail.com>
2025-04-09 20:30:41 +0300

    Create Exm_1.js

---
+++ Практическое занятие №2-1/Exm_1.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>lab-1-1</title>
+</head>
+<body>
+    <h1>lab-1-1</h1>
+    <p>Example 1</p>
+
+    <script>
+        // Creating variables string1 and string2 and concatenating strings
+        let string1 = "Hello, ";
+        let string2 = "world!";
+        let combinedString = string1 + string2;
+        alert("Concatenated string: " + combinedString);
+
+        // Creating variables 3.14 and FF, then multiplying them
+        let num1 = 3.14;
+        let hexValue = 0xFF; // FF in hexadecimal = 255 in decimal
+        let multiplicationResult = num1 * hexValue;
+        alert("Multiplication result: " + multiplicationResult);
+    </script>
+</body>
+</html>
